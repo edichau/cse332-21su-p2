582d6121d98960a53da69b6e1320017461d3be59
Lauren <lecavs91@msn.com>
2021-07-13 22:02:39 -0700

    changed implementation to linkedList. add iterator. Passes P1 tests.

--- src/datastructures/dictionaries/MoveToFrontList.java
+++ src/datastructures/dictionaries/MoveToFrontList.java
@@ -1,10 +1,15 @@
 package datastructures.dictionaries;
 
 import cse332.datastructures.containers.Item;
+import cse332.datastructures.trees.BinarySearchTree;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
 import cse332.interfaces.misc.Dictionary;
+import cse332.interfaces.misc.SimpleIterator;
+import cse332.interfaces.worklists.WorkList;
 import datastructures.worklists.ArrayStack;
+import datastructures.worklists.CircularArrayFIFOQueue;
+import datastructures.worklists.ListFIFOQueue;
 
 import java.util.Iterator;
 
@@ -22,11 +27,26 @@ import java.util.Iterator;
  *    dictionary/list's iterator. 
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
-    private ArrayStack<Item<K, V>> stack;
+    private Node root;
+
+    private class Node extends Item<K,V>  {
+        public Node next;
+
+        public Node(K key, V value) {
+            super(key, value);
+            next = null;
+        }
+
+        public Node(K key, V value, Node next) {
+            super(key, value);
+            this.next = next;
+        }
+    }
 
     public MoveToFrontList() {
         super();
-        this.stack = new ArrayStack<>();
+        this.root = null;
+        this.size = 0;
     }
 
     @Override
@@ -35,13 +55,26 @@ public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
             throw new IllegalArgumentException();
         }
 
-        V oldVal = find(key); //key is now at top of stack from searching. If key doesn't already exist value is null
-        if(oldVal != null) {
-            stack.peek().value = value;
+        if(root == null) {
+            //List is empty so we insert at root
+            root = new Node(key, value, null);
+            size++;
+            return null;
         } else {
-            stack.add(new Item<>(key,value));
+            //List is not empty so we search if key already exists
+            V oldVal = find(key); //returns old val if previously in list. null ow.
+            if (oldVal != null) {
+                //key already exists and is now at root
+                root.value = value;
+            } else {
+                //key doesnt exist. Add a new <key,value> that links to previous root
+                Node newNode = new Node(key, value);
+                newNode.next = root;
+                root= newNode;
+                size++;
+            }
+            return oldVal;
         }
-        return oldVal;
     }
 
     @Override
@@ -49,33 +82,59 @@ public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
         if(key == null) {
             throw new IllegalArgumentException();
         }
-        ArrayStack<Item<K,V>> temp = new ArrayStack<>();
-        boolean foundItem = false;
-        Item<K, V> value = new Item<>(key, null);
+        Node prev = null;
+        Node curr = this.root;
 
-        //iterate through stack pushing values to temp to preserve order
-        while(stack.hasWork() && !foundItem) {
-            Item<K, V> next = stack.next();
-            if(next.key.equals(key)) {
-                foundItem = true;
-                value = next;
+        //root is null so list is empty
+        if(curr == null) {
+            return null;
+        }
+
+        //else we iterate through list til key is found or end of list
+        while(curr != null && !curr.key.equals(key)) {
+            prev = curr;
+            curr = curr.next;
+        }
+        if(curr == null) {
+            //value doesnt exist
+            return null;
+        } else {
+            //value is in list
+            if(prev != null) {
+                //value is in middle of list
+                prev.next = curr.next;
+                curr.next = root;
+                root = curr;
             } else {
-                temp.add(next);
+                //value is already at the root
+                return root.value;
             }
+            return curr.value;
         }
-        //push values back onto stack in the order they were removed starting with most recently removed
-        while(temp.hasWork()) {
-            stack.add(temp.next());
-        }
-        //if in list, we put key to front with new value
-        if(value.value != null) {
-            stack.add(value);
-        }
-        return value.value;
     }
 
     @Override
-    public Iterator<Item<K, V>> iterator() {
-        return null;
+    public Iterator<Item<K, V>> iterator() { return new NodeIterator(); }
+
+
+    private class NodeIterator extends SimpleIterator<Item<K, V>> {
+        private Node current;
+
+        public NodeIterator() {
+            this.current = MoveToFrontList.this.root;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.current != null;
+        }
+
+        @Override
+        public Item<K, V> next() {
+            Item<K,V> retVal = new Item<>(this.current.key, this.current.value);
+            this.current= this.current.next;
+            return retVal;
+        }
     }
+
 }
