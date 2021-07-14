03df03387a77489d61f80184e0968a15092e4313
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 18:06:13 -0700

    Completed MinFourHeapComparable methods, passes p1 tests

--- src/datastructures/worklists/MinFourHeapComparable.java
+++ src/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,8 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -10,38 +12,101 @@ import cse332.interfaces.worklists.PriorityWorkList;
 public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int size;
 
+    @SuppressWarnings("unchecked")
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+        this.data = (E[]) new Comparable[20];
+        this.size = 0;
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        return this.size() > 0;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (this.size == this.data.length) {
+            resize();
+        }
+        this.data[this.size++] = work;
+        this.percolateUp();
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (!this.hasWork()) {
+            throw new NoSuchElementException();
+        }
+        return this.data[0];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (!this.hasWork()) {
+            throw new NoSuchElementException();
+        }
+
+        E minElem = this.data[0];
+
+        this.data[0] = this.data[--this.size];
+        percolateDown(0);
+
+        return minElem;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void clear() {
-        throw new NotYetImplementedException();
+        this.data = (E[]) new Comparable[20];
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        E[] copy = (E[]) new Comparable[this.data.length * 2];
+        if (this.size >= 0) System.arraycopy(this.data, 0, copy, 0, this.size);
+        this.data = copy;
+    }
+
+    private void percolateUp()  {
+        for (int i = size-1; i > 0; i--) {
+            E child = data[i];
+            E parent = data [(i-1)/4];
+
+            if (child.compareTo(parent) < 0) {
+                data[(i-1)/4] = child;
+                data[i] = parent;
+            }
+        }
+    }
+
+    private void percolateDown(int index) {
+        int minChildIndex = index * 4 + 1;
+
+        if (minChildIndex >= this.size) {
+            return;
+        }
+
+        int childIndex = index * 4 + 2;
+        while (childIndex <= index * 4 + 4 && childIndex < this.size) {
+            if (this.data[childIndex].compareTo(this.data[minChildIndex]) < 0) {
+                minChildIndex = childIndex;
+            }
+            childIndex++;
+        }
+
+        if (this.data[index].compareTo(this.data[minChildIndex]) > 0) {
+            E tmp = this.data[index];
+            this.data[index] = this.data[minChildIndex];
+            this.data[minChildIndex] = tmp;
+            percolateDown(minChildIndex);
+        }
     }
 }
