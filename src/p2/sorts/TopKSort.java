df4504f6eb3d462ca67532d4aa001189f35663b2
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 02:45:32 -0700

    passed all tests for MinFourHeap and 2 sorts, TODO: quicksort

--- src/p2/sorts/TopKSort.java
+++ src/p2/sorts/TopKSort.java
@@ -1,6 +1,7 @@
 package p2.sorts;
 
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 import java.util.Comparator;
 
@@ -10,6 +11,22 @@ public class TopKSort {
     }
 
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap<E> heap = new MinFourHeap<>(comparator);
+        for (int i = 0; i < array.length; i++) {
+            if (heap.size() < k) {
+                heap.add(array[i]);
+            } else if (comparator.compare(array[i], heap.peek()) > 0) {
+                heap.next();
+                heap.add(array[i]);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (i < k) {
+                array[i] = heap.next();
+            } else {
+                array[i] = null;
+            }
+        }
     }
 }
